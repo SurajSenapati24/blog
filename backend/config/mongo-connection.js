6b4b7918865b37ae1f7b583215e330055bf378c7
Suraj Senapati <surajsenapati58@gmail.com>
2025-04-11 23:06:58 +0530

    update

--- backend/config/mongo-connection.js
+++ backend/config/mongo-connection.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
+const mongooseURI = process.env.MONGODB_URL;
 
-const mongooseURI = process.env.MONGODB_URI;
-
-mongoose.connect(mongooseURI)
+mongoose.connect(mongooseURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
 .then(() => console.log('Connected to MongoDB'))
 .catch(err => console.error('Could not connect to MongoDB', err));
 
